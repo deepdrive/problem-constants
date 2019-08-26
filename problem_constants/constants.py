a0f6fed5f228d53a18a9fdb1498b6e267c813dd0
Craig Quiter <cquiter@gmail.com>
2019-08-26 16:20:18 -0700

    Handle more than eval in problem service job manager

--- problem_constants/constants.py
+++ problem_constants/constants.py
@@ -13,17 +13,17 @@ GCP_PROJECT = os.environ.get('GOOGLE_CLOUD_PROJECT') or \
               os.environ.get('GCP_PROJECT') or 'silken-impulse-217423'
 
 STACKDRIVER_LOG_NAME = 'deepdrive-problem-endpoint'
-INSTANCE_EVAL_LABEL = 'deepdrive-eval'
-EVAL_INSTANCES_COLLECTION_NAME = 'deepdrive_eval_instances'
-EVAL_JOBS_COLLECTION_NAME = 'deepdrive_eval_jobs'
+WORKER_INSTANCE_LABEL = 'deepdrive-worker'
+WORKER_INSTANCES_COLLECTION_NAME = 'deepdrive_worker_instances'
+JOBS_COLLECTION_NAME = 'deepdrive_jobs'
 EVAL_CONFIG_COLLECTION_NAME = 'deepdrive_eval_config'
-EVAL_LOOP_ID = 'deepdrive_eval_loop'
+JOB_LOOP_ID = 'deepdrive_job_loop'
 
 CONTAINER_RUN_OPTIONS = dict(runtime='nvidia', network='host')
 
 # Needs to be divisible by 2 as we start a problem and bot instance for each
 # eval
-MAX_EVAL_INSTANCES = 6
+MAX_WORKER_INSTANCES = 6
 MAX_EVAL_SECONDS_DEFAULT = 5 * 60
 
 # This should be public for submitters to see logs
@@ -36,6 +36,9 @@ BOTLEAGUE_RESULTS_FILEPATH = f'{BOTLEAGUE_RESULTS_DIR}/results.json'
 BOTLEAGUE_LIAISON_HOST = os.environ.get('BOTLEAGUE_LIAISON_HOST',
                                         default='https://liaison.botleague.io')
 
+JOB_TYPE_EVAL = 'eval'
+JOB_TYPE_SIM_BUILD = 'sim-build'
+
 JOB_STATUS_CREATED = 'created'
 JOB_STATUS_CONFIRMED = 'confirmed'
 JOB_STATUS_ASSIGNED = 'assigned'
@@ -47,8 +50,8 @@ JOB_STATUS_UNSUPPORTED_PROBLEM = 'unsupported_problem'
 
 INSTANCE_STATUS_AVAILABLE = 'available'
 INSTANCE_STATUS_USED = 'used'
-INSTANCE_CONFIG_PATH = 'cloud_configs/eval_instance_create.json'
-INSTANCE_NAME_PREFIX = 'deepdrive-eval-problem-worker-'
+INSTANCE_CONFIG_PATH = 'cloud_configs/worker_instance_create.json'
+INSTANCE_NAME_PREFIX = 'deepdrive-worker-'
 
 METADATA_URL = 'http://metadata.google.internal/computeMetadata/v1/instance'
 
